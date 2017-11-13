08d839c4af4ac1f8e2a6f4fdac3a71ab7d782c12
rokymiel <rokymiel@gmail.com>
2017-11-13 19:39:12 +0300

    Plus and minus

--- app/src/main/java/com/example/student2/calculator/MainActivity.java
+++ app/src/main/java/com/example/student2/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.student2.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void plus (View view){
+        EditText num1 =(EditText) findViewById(R.id.num1);
+        String num1text=num1.getText().toString();
+        EditText num2 =(EditText) findViewById(R.id.num2);
+        String num2text=num2.getText().toString();
+        TextView out =(TextView) findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text)+Double.parseDouble(num2text)));
+
+    }
+
+    public void minus(View view) {
+        EditText num1 =(EditText) findViewById(R.id.num1);
+        String num1text=num1.getText().toString();
+        EditText num2 =(EditText) findViewById(R.id.num2);
+        String num2text=num2.getText().toString();
+        TextView out =(TextView) findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(num1text)-Double.parseDouble(num2text)));
+
     }
 }
